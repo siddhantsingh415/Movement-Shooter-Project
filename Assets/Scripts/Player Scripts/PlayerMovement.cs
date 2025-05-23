979ac4b6a8f543f65a3f0dcf781ac2d68793e668
siddhantsingh415 <88722584+siddhantsingh415@users.noreply.github.com>
2025-05-23 15:26:12 -0400

    Basic Dash Implemented
    need to update to allow for double dashing and making it smoother

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,8 +12,14 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private MouseSensitivity mouseSensitivity;
     private CamerRotation cameraRotation;
     [SerializeField] private CameraAngle cameraAngle;
+    bool canDash = true;
+    bool isDashing = false;
+    public float dashingPower = 200f;
+    private float dashDuration = 0.3f;
+    private float dashCooldown = 1f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Start is called once before the first execution of Update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -26,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
     {
         movePlayer();
         look();
+        dodgeDash();
     }
 
     void movePlayer()
@@ -43,6 +52,31 @@ public class PlayerMovement : MonoBehaviour
         cameraRotation.pitch = Mathf.Clamp(cameraRotation.pitch, cameraAngle.min, cameraAngle.max);
         mainCamera.transform.eulerAngles = new Vector3(cameraRotation.pitch, cameraRotation.yaw, 0.0f);
         transform.eulerAngles = new Vector3(0.0f, cameraRotation.yaw, 0.0f);
+    }
+    private void dodgeDash()
+    {
+        if (canDash)
+        {
+            float dashinput = playerInput.actions["Dash"].ReadValue<float>();
+            if (dashinput > 0)
+            {
+                StartCoroutine(Dash());
+            }
+        }
+    }
+    // redo tomorrow to have two coroutienes 1 to control the dash and one to control cooldown for dash charges
+    private IEnumerator Dash()
+    {
+        Vector3 movement = new Vector3(playerInput.actions["Move"].ReadValue<Vector2>().x, 0, playerInput.actions["Move"].ReadValue<Vector2>().y);
+        canDash = false;
+        isDashing = true;
+        rb.linearVelocity += (transform.TransformDirection(movement) * dashingPower);
+
+        yield return new WaitForSeconds(dashDuration);
+        rb.linearVelocity = Vector3.zero;
+        isDashing = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
 
